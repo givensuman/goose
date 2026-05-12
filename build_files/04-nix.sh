d7dbbd1abf81d35bbf8dc676a39723c84a5519ab
givensuman <givensuman@duck.com>
2026-05-12 15:51:30 -0400

    Change Nix installation

--- build_files/04-nix.sh
+++ build_files/04-nix.sh
@@ -11,25 +11,35 @@ trap 'log_error "Script failed at line $LINENO"' ERR
 
 log_info "Setting up nix..."
 
-log_info "Enabling transient root..."
-tee /etc/ostree/prepare-root.conf <<'EOL'
-[composefs]
-enabled = yes
-[root]
-transient = true
-EOL
+# Use Determinate Systems installer
+curl --proto '=https' --tlsv1.2 -sSf -L https://determinate.systems | sh -s -- install
 
-rpm-ostree initramfs-etc --reboot --track=/etc/ostree/prepare-root.conf
+nix-channel --add \
+  https://github.com/nix-community/home-manager/archive/master.tar.gz \
+  home-manager
 
-log_info "Installing nix"
-curl -sSfL https://artifacts.nixos.org/nix-installer | \
-    sh -s -- install ostree --explain --add-channel --persistence=/var/lib/nix
-
-log_info "Add nix unstable channel"
+# log_info "Enabling transient root..."
+# tee /etc/ostree/prepare-root.conf <<'EOL'
+# [composefs]
+# enabled = yes
+# [root]
+# transient = true
+# EOL
+#
+# rpm-ostree initramfs-etc --reboot --track=/etc/ostree/prepare-root.conf
+#
+# log_info "Installing nix"
+# curl -sSfL https://artifacts.nixos.org/nix-installer | \
+#     sh -s -- install ostree --explain --add-channel --persistence=/var/lib/nix
+#
+# log_info "Add nix unstable channel"
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
 nix-channel --update
 
-log_info "Fix sudo"
-echo "Defaults  secure_path = /nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:$(sudo printenv PATH)" | sudo tee /etc/sudoers.d/nix-sudo-env
+# log_info "Fix sudo"
+# echo "Defaults  secure_path = /nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:$(sudo printenv PATH)" | sudo tee /etc/sudoers.d/nix-sudo-env
+
+# Install home-manager
+nix-shell '<home-manager>' -A install
 
 log_info "Nix setup completed successfully"
